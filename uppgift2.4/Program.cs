9267dbd898dba7df15cc49ba0eaa3c71d57a096e
emsj06001 <emsj06001@E-PW024NWP>
2023-09-15 12:45:04 +0200

    Add project files.

---
+++ uppgift2.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uppgift_2_4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in lönen på den första personen...");
+            string x = Console.ReadLine();
+            Console.WriteLine("Skriv in lönen på den andra personen...");
+            string y = Console.ReadLine();
+            Console.WriteLine("Skriv in lönen på den tredje personen...");
+            string z= Console.ReadLine();
+
+            int lön1 = int.Parse(x);
+            int lön2 = int.Parse(y);
+            int lön3 = int.Parse(z);
+            int medel = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medelvärdet på dessa löner blir "+ medel +" kr.");
+            Console.ReadKey();
+        }
+    }
+}
